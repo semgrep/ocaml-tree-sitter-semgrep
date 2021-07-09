8d6dc1ab95afa5e0733e93afaeffa3b7ad3783b1
pad <yoann.padioleau@gmail.com>
2021-07-09 16:01:42 +0200

    [Semgrep-html] allow metavariables for tags
    test plan:
    ./test-lang html

--- lang/semgrep-grammars/src/semgrep-html/grammar.js
+++ lang/semgrep-grammars/src/semgrep-html/grammar.js
@@ -17,15 +17,51 @@ module.exports = grammar(base_grammar, {
      if they're not already part of the base grammar.
   */
   rules: {
+
+/* pad: not sure why, but this is simpler than patching element but
+   it does not work. I get some 
+
+       "The rule `start_tag` matches the empty string" tree-sitter
+
+   error.
+
+    start_tag: ($, previous) => {
+      return choice(
+       $.semgrep_start_tag,
+       ...previous.members
+      );
+    },
+*/
+    element: ($, previous) => {
+      return choice(
+       $.semgrep_element,
+       ...previous.members
+      );
+    },
+
+    semgrep_element: $ => 
+     seq(
+       $._semgrep_start_tag,
+       repeat($._node),
+       $._semgrep_end_tag
+     ),
+
+    _semgrep_start_tag: $ => seq(
+        '<',
+        $._semgrep_metavariable,
+        repeat($.attribute),
+        '/>'
+     ),
+    _semgrep_end_tag: $ => seq(
+     '</',
+     $._semgrep_metavariable,
+     '>'
+   ),
+
+    _semgrep_metavariable: $ => token(/\$[A-Z_][A-Z_0-9]*/),
+
   /*
     semgrep_ellipsis: $ => '...',
-
-    _expression: ($, previous) => {
-      return choice(
-        $.semgrep_ellipsis,
-        ...previous.members
-      );
-    }
   */
   }
 });
