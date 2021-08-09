f9065ac7c5a7bdf1e8d891870ec5af78d66a3dbb
David Frankel <42774874+frankeld@users.noreply.github.com>
2021-08-09 19:01:01 -0400

    Extend statements and expressions with ellipsis (still need to update TSH)

--- lang/semgrep-grammars/src/semgrep-hack/grammar.js
+++ lang/semgrep-grammars/src/semgrep-hack/grammar.js
@@ -13,23 +13,58 @@ const base_grammar = require('tree-sitter-hacklang/grammar');
 module.exports = grammar(base_grammar, {
   name: 'hack',
 
-  conflicts: ($, previous) => previous.concat([
-  ]),
+  conflicts: ($, previous) => previous.concat([]),
 
-  /*
-     Support for semgrep ellipsis ('...') and metavariables ('$FOO'),
-     if they're not already part of the base grammar.
-  */
   rules: {
-  /*
-    semgrep_ellipsis: $ => '...',
+    /*
+      Support for semgrep ellipsis ('...')
+    */
+
+    // For now, we give precedence to native ellipsis (aka variadic modifiers).
+    ellipsis: ($) => prec(-1, '...'),
+    deep_ellipsis: ($) => prec(-1, seq('<...', $._expression, '...>')),
+
+    // By using an empty statement, we leverage a statement type that is strictly only used within
+    // $._statement and nowhere else.
+    empty_statement: ($, previous) => {
+      return choice(
+        previous,
+        // For conflict with `ellipsis;`
+        // we want it to be read as an expression with a semicolon
+        // instead of a statement
+        prec(-1, $.ellipsis) // statement ellipsis
+      );
+    },
 
     _expression: ($, previous) => {
-      return choice(
-        $.semgrep_ellipsis,
-        ...previous.members
+      return choice(previous, $.ellipsis, $.deep_ellipsis);
+    },
+
+    member_declarations: ($) => {
+      return seq(
+        '{',
+        choice.rep(
+          // Copied from existing grammar
+          alias($._class_const_declaration, $.const_declaration),
+          $.method_declaration,
+          $.property_declaration,
+          $.type_const_declaration,
+          $.trait_use_clause,
+          $.require_implements_clause,
+          $.require_extends_clause,
+          $.xhp_attribute_declaration,
+          $.xhp_children_declaration,
+          $.xhp_category_declaration,
+
+          // Additions
+          $.ellipsis
+        ),
+        '}'
       );
-    }
-  */
-  }
+    },
+
+    parameter: ($, previous) => {
+      return choice(previous, $.ellipsis);
+    },
+  },
 });
