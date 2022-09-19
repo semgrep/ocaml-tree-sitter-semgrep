3a7e08725596b5f88356c2fed99f35892d7e10a8
pad <pad@r2c.dev>
2022-09-19 13:31:04 +0200

    C++: start support semgrep patterns (metavar and simple ellipsis)
    This will help https://github.com/returntocorp/semgrep/issues/1923
    
    test plan:
    test file included
    ./test-lang cpp

--- lang/semgrep-grammars/src/semgrep-cpp/grammar.js
+++ lang/semgrep-grammars/src/semgrep-cpp/grammar.js
@@ -10,6 +10,10 @@ module.exports = grammar(base_grammar, {
   name: 'cpp',
 
   conflicts: ($, previous) => previous.concat([
+      // C++ allows 'sizeof ...(id)' hence the conflict
+      [$.sizeof_expression, $.semgrep_ellipsis],
+      // C allows ... in parameters
+      [$.parameter_list, $.semgrep_ellipsis],
   ]),
 
   /*
@@ -17,15 +21,30 @@ module.exports = grammar(base_grammar, {
      if they're not already part of the base grammar.
   */
   rules: {
-  /*
-    semgrep_ellipsis: $ => '...',
+    // Metavariables
+
+    /* we can't use the usual:
+
+	 identifier: ($, prev) => { return choice(prev, $._semgrep_metavariable);},
+         _semgrep_metavariable: $ => /\$[A-Z_][A-Z_0-9]* /,
+
+      because in tree-sitter-c 'identifier' is used in the 'word:' directive and
+      'identifier' then can't be a non-terminal. In other languages this is solved
+      by having 'identifier' and a separate '_identifier_token' (e.g., in C#)
+    */
+    identifier: $ => /\$?[a-zA-Z_]\w*/, // original = /[a-zA-Z_]\w*/
+
+    // Ellipsis
 
     _expression: ($, previous) => {
       return choice(
+        previous,
         $.semgrep_ellipsis,
-        ...previous.members
+        $.deep_ellipsis,
       );
-    }
-  */
+    },
+	
+    semgrep_ellipsis: $ => '...',
+    deep_ellipsis: $ => seq('<...', $._expression, '...>'),
   }
 });
