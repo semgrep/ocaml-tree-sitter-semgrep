f3347052760e25d606782bf91e0769a5ce5f6023
pad <pad@r2c.dev>
2023-02-03 12:30:47 +0100

    XML: add support for xml by tweaking semgrep-html
    test plan:
    ./test-lang html
    ./html/parse-html ~/pom.xml
    which works

--- lang/semgrep-grammars/src/semgrep-html/grammar.js
+++ lang/semgrep-grammars/src/semgrep-html/grammar.js
@@ -1,7 +1,12 @@
 /*
   semgrep-html
 
-  Extends the standard html grammar with semgrep pattern constructs.
+  Extends the standard HTML grammar with Semgrep pattern constructs.
+
+  It now also extend the HTML grammar with XML constructs! See scanner_cc.diff
+  for the extension to the scanner to support XML entity names (e.g., <f:bar></f:bar>).
+  An alternative would be to switch to https://github.com/unhammer/tree-sitter-xml,
+  but its grammar looks very complicated and the code is not maintained.
 */
 
 const base_grammar = require('tree-sitter-html/grammar');
@@ -17,7 +22,20 @@ module.exports = grammar(base_grammar, {
      valid HTML syntax in many places, so we don't need that many
      grammar extensions
   */
-  rules: {
+    rules: {
+
+    // XML extensions
+    _node: ($, previous) => choice(
+       $.xmldoctype,
+       previous
+    ),
+    
+    xmldoctype: $ => seq(
+      '<?xml',
+      repeat($.attribute),
+      '?>'
+    ),
+	
     //alt: redefine instead _start_tag_name and _end_tag_name?
     start_tag: ($, previous) => choice(
       $.semgrep_start_tag,
@@ -42,6 +60,5 @@ module.exports = grammar(base_grammar, {
     ),
 
     semgrep_metavariable: $ => token(/\$[A-Z_][A-Z_0-9]*/),
-      
   }
 });
