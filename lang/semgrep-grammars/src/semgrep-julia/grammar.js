ae62c9ef003c1034de471157f790c49cb0507ad5
Michael Hoffmann <mhoffm@posteo.de>
2023-08-09 18:08:15 +0200

    feat(julia): allow metavariables for import statements

--- lang/semgrep-grammars/src/semgrep-julia/grammar.js
+++ lang/semgrep-grammars/src/semgrep-julia/grammar.js
@@ -4,42 +4,41 @@
   Extends the standard julia grammar with semgrep pattern constructs.
 */
 
-const base_grammar = require('tree-sitter-julia/grammar');
+const base_grammar = require("tree-sitter-julia/grammar");
 
 module.exports = grammar(base_grammar, {
-  name: 'julia',
+  name: "julia",
 
-  conflicts: ($, previous) => previous.concat([
-    [$.typed_parameter, $._expression],
-  ]),
+  conflicts: ($, previous) =>
+    previous.concat([[$.typed_parameter, $._expression]]),
 
   /*
      Support for semgrep ellipsis ('...') and metavariables ('$FOO'),
      if they're not already part of the base grammar.
   */
   rules: {
-    semgrep_ellipsis: $ => '...',
+    semgrep_ellipsis: ($) => "...",
+    _semgrep_metavariable: (_) => token(/\$[A-Z_][A-Z_0-9]*/),
 
-    catch_clause: $ => prec(1, seq(
-      'catch',
-      optional(choice($.identifier, alias($.semgrep_ellipsis, $.catch_ellipsis))),
-      optional($._terminator),
-      optional($._block),
-    )),
+    catch_clause: ($) =>
+      prec(
+        1,
+        seq(
+          "catch",
+          optional(
+            choice($.identifier, alias($.semgrep_ellipsis, $.catch_ellipsis)),
+          ),
+          optional($._terminator),
+          optional($._block),
+        ),
+      ),
 
-    _expression: ($, previous) => choice(
-      previous,
-      $.semgrep_ellipsis,
-    ),
+    _expression: ($, previous) => choice(previous, $.semgrep_ellipsis),
 
-    _statement: ($, previous) => choice(
-      previous,
-      $.semgrep_ellipsis,
-    ),
+    _statement: ($, previous) => choice(previous, $.semgrep_ellipsis),
 
-    typed_parameter: ($, previous) => choice(
-      previous,
-      $.semgrep_ellipsis,
-    ),
-  }
+    typed_parameter: ($, previous) => choice(previous, $.semgrep_ellipsis),
+
+    _importable: ($, previous) => choice(previous, $._semgrep_metavariable),
+  },
 });
