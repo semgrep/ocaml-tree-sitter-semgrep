4ad49d6035f43a56c2b250765156cfa0c4f3004e
Rahul Nair <rahuln@mystenlabs.com>
2024-06-22 12:09:04 -0700

    update

---
+++ lang/semgrep-grammars/src/semgrep-move/grammar.js
@@ -0,0 +1,29 @@
+/*
+  semgrep-move
+
+  Extends the standard move grammar with semgrep pattern constructs.
+*/
+
+const base_grammar = require('tree-sitter-move/grammar');
+
+module.exports = grammar(base_grammar, {
+  name: 'move',
+
+  conflicts: ($, previous) => previous.concat([
+  ]),
+
+  /*
+     Support for semgrep ellipsis ('...') and metavariables ('$FOO'),
+     if they're not already part of the base grammar.
+  */
+  rules: {
+  /*
+    semgrep_ellipsis: $ => '...',
+
+    _expression: ($, previous) => choice(
+      $.semgrep_ellipsis,
+      ...previous.members
+    ),
+  */
+  }
+});
