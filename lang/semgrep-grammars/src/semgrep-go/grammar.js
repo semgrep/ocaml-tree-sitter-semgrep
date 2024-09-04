c2a3f890b4e1a212290b5e66a3abd3f6acf69fba
Brandon Wu <49291449+brandonspark@users.noreply.github.com>
2024-09-04 13:57:25 -0700

    feat(go): basic semgrep support (#504)
    * feat: semgrep support for go
    
    * add typed metavars

--- lang/semgrep-grammars/src/semgrep-go/grammar.js
+++ lang/semgrep-grammars/src/semgrep-go/grammar.js
@@ -17,15 +17,38 @@ module.exports = grammar(base_grammar, {
      if they're not already part of the base grammar.
   */
   rules: {
-  /*
-    semgrep_ellipsis: $ => '...',
+    semgrep_ellipsis: $ => "...",
 
-    _expression: ($, previous) => {
-      return choice(
-        $.semgrep_ellipsis,
-        ...previous.members
-      );
-    }
-  */
+    semgrep_ellipsis_metavar : $ => /\$\.\.\.[a-zA-Z_][a-zA-Z_0-9]*/,
+    semgrep_deep_ellipsis: $ => seq("<...", $._expression, "...>"),
+
+    _expression: ($, previous) => choice(
+      previous,
+      $.semgrep_ellipsis_metavar,
+      $.semgrep_deep_ellipsis,
+      $.semgrep_ellipsis,
+      $.typed_metavar
+    ),
+
+    typed_metavar: $ => seq(
+      "(", $.identifier, ":", $._type, ")"
+    ),
+
+    identifier: ($, previous) => token(choice(
+      previous,
+      // inline this here so we can stay inside of the `token`, because
+      // `identifier` is the word token
+      /\$[A-Z_][A-Z_0-9]*/
+    )),
+
+    parameter_declaration: ($, previous) => choice(
+      $.semgrep_ellipsis,
+      $.semgrep_ellipsis_metavar,
+      previous
+    ),
+
+    // slightly more precedence so we bump this up over using `...`
+    // for a semgrep ellipsis
+    implicit_length_array_type: ($, previous) => prec(1, previous)
   }
 });
