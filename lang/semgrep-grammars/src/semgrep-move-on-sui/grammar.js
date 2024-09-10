2ecb3fdbb318b18c5cc1d50250119d7c8a0d251a
maxmysten <max.spector@mystenlabs.com>
2024-09-09 21:45:43 -0400

    Fixing Macros so now macro variables that start with $ parse.

--- lang/semgrep-grammars/src/semgrep-move-on-sui/grammar.js
+++ lang/semgrep-grammars/src/semgrep-move-on-sui/grammar.js
@@ -21,6 +21,7 @@ module.exports = grammar(base_grammar, {
     [$.module_body],
     [$._expression_term],
     [$.semgrep_statement,$._expression_term],
+    [$.typed_metavariable, $.annotation_expression],
   ]),
 
   precedences: ($, previous) => previous.concat([
@@ -33,7 +34,9 @@ module.exports = grammar(base_grammar, {
     [$.module_access, $.field_access_ellipsis_expr],
     [$.bind_unpack, $.name_expression],
     [$.type_arguments, $._type, $.module_access],
-    [$.typed_metavariable, $.annotation_expression],
+    [$.annotation_expression, $.typed_metavariable],
+    [$._semgrep_metavar_ellipsis, $.typed_metavariable],
+
   ]),
 
   /*
@@ -45,14 +48,22 @@ module.exports = grammar(base_grammar, {
     ellipsis: $ => '...',
     deep_ellipsis: $ => seq('<...',$._expression, '...>'),
     //_semgrep_metavar_ellipsis: $ => /\$\.\.\.[A-Z_][A-Z_0-9]*/,
-    //_semgrep_metavariable: $ => /\$[A-Z_][A-Z_0-9]*/,
+    _semgrep_metavariable_after_dollar: $ => /[A-Z_][A-Z_0-9]*/,
 
 
     // Typed metavariable (an expression, not a parameter)
     // This is grammatically indistinguishable from `$.type_hint_expr: $ => seq('(', $._expr, ':', $.type, ')')`.
     // This will be handled by the semgrep converter by checking the metavariable name (`$`).
 
-    typed_metavariable: $ => seq('(', alias(choice(/\$[A-Z_][A-Z_0-9]*/,  /\$\.\.\.[A-Z_][A-Z_0-9]*/), $.identifier), ':', $._type, ')'),
+
+    typed_metavariable: $ => seq('(',  $.identifier, ':', $._type, ')'),
+
+    _clean_identifier: $ => /(`)?[a-zA-Z_][0-9a-zA-Z_]*(`)?/,
+    _clean_macro_identifier : $ => /[a-zA-Z][0-9a-zA-Z_]*/,
+
+    _macro_identifier_dollar : $ => /\$[a-zA-Z][0-9a-zA-Z_]*/,
+    _semgrep_metavar_ellipsis: $ => /\$\.\.\.[A-Z_][A-Z_0-9]*/,
+
 
     // Alternate "entry point". Allows parsing a standalone expression.
     semgrep_expression: $ => choice(
@@ -90,15 +101,48 @@ module.exports = grammar(base_grammar, {
       $.semgrep_partial,
     ),
 
+    _identifier_or_metavariable: ($, previous) =>  choice(
+        seq('$', prec.right(
+            choice(
+              $._clean_macro_identifier,
+              seq('...', $._semgrep_metavariable_after_dollar),
+              $._semgrep_metavariable_after_dollar,
+          )
+        )),
+        seq(
+          optional('phantom'),
+          $._clean_identifier,
+        )
+    ),
+
     // Module declaration
     module_body: ($, previous) => choice(
       previous,
       $.ellipsis,
     ),
 
-    module_access: ($, previous) => choice( 
-      previous,
-      $.ellipsis
+    module_access: ($, previous)  => choice(
+      // macro variable access
+      field('member', alias(prec.right(choice(
+          $._semgrep_metavar_ellipsis,
+          $._macro_identifier_dollar,
+          prec(-1,$.identifier),
+        )),  $.identifier)
+      ),
+      // address access
+      seq('@', field('member', $.identifier)),
+      field('member', alias($._reserved_identifier, $.identifier)),
+      seq(
+        field('module', $._module_identifier),
+        '::',
+        field('member', $.identifier)
+      ),
+      seq(
+        $.module_identity,
+        '::',
+        field('member', $.identifier)
+      ),
+      seq($.module_identity, '::', field('enum_name', $.identifier), '::', field('variant', $.identifier)),
     ),
 
     // Spec block members
@@ -152,6 +196,7 @@ module.exports = grammar(base_grammar, {
       previous,
       $.ellipsis,
       $.deep_ellipsis,
+      $.typed_metavariable,
     ),
 
     // expression 
@@ -167,8 +212,6 @@ module.exports = grammar(base_grammar, {
       prec(UNARY_PREC, $.ellipsis),
       prec(UNARY_PREC, $.deep_ellipsis),
       prec(UNARY_PREC, $.field_access_ellipsis_expr),
-      prec(UNARY_PREC+1,$.typed_metavariable),
-
     ),
     _dot_or_index_chain: ($, previous) => choice(
       previous,
@@ -178,7 +221,12 @@ module.exports = grammar(base_grammar, {
     // function parameter
     // (e.g. `call( ..., arg, ...)`)
     function_parameter: ($, previous) => choice(
-      previous,
+      seq(
+        optional('mut'),
+        field('name', alias($._identifier_or_metavariable, $.variable_identifier)),
+        ':',
+        field('type', $._type),
+      ),
       $.ellipsis,
     ),
 
@@ -199,9 +247,15 @@ module.exports = grammar(base_grammar, {
     // type parameter
     // (e.g. `Type<..., T>`)
     type_parameter: ($, previous) => choice(
-      previous,
-      $.ellipsis,
+      seq(
+        alias($._identifier_or_metavariable, $.type_parameter_identifier),
+        optional(seq(':',
+          sepBy1('+', $.ability)
+        )),
+      ),
+        $.ellipsis,
     ),
+
 
     // type arguments
     // (e.g. `Type<..., T>`)
@@ -222,15 +276,18 @@ module.exports = grammar(base_grammar, {
 
     field_access_ellipsis_expr: $ => prec.left(FIELD_PREC, seq(
       field('element', $._dot_or_index_chain), '.',$.ellipsis )),
-    // identifier, extended to support metavariables
-    identifier: ($, previous) => token(prec(-1,choice(
-      prec(100,previous),
-      // Metavariables
-      prec(-1,alias(
-        /\$[A-Z_][A-Z_0-9]*/,$.meta_var)), 
-      prec(300,alias(
-          /\$\.\.\.[A-Z_][A-Z_0-9]*/,$.metavar_ellipsis)), 
-        
-    ))),
+ 
+    identifier: $ => /(`)?[a-zA-Z_][0-9a-zA-Z_]*(`)?|\$\.\.\.[A-Z_][A-Z_0-9]*]|\$[A-Z_][A-Z_0-9]*/,
+
   }
 });
+
+
+//      (<rule> 'sep')* <rule>?
+// Note that this allows an optional trailing `sep`.
+function sepBy(sep, rule) {
+  return seq(repeat(seq(rule, sep)), optional(rule));
+}
+function sepBy1(sep, rule) {
+  return seq(rule, repeat(seq(sep, rule)), optional(sep));
+}
