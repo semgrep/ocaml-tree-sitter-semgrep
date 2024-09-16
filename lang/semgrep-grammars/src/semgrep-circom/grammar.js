c1289db74c761bc56dc208fd999fa8c10beb7acd
frodan <fronc2000@gmail.com>
2024-09-16 17:41:35 +0700

    update circom semgrep grammar

--- lang/semgrep-grammars/src/semgrep-circom/grammar.js
+++ lang/semgrep-grammars/src/semgrep-circom/grammar.js
@@ -12,10 +12,6 @@ module.exports = grammar(base_grammar, {
   conflicts: ($, previous) => previous.concat([
   ]),
 
-  /*
-     Support for semgrep ellipsis ('...') and metavariables ('$FOO'),
-     if they're not already part of the base grammar.
-  */
   rules: {
 
     source_file: ($, previous) => {
@@ -39,10 +35,39 @@ module.exports = grammar(base_grammar, {
     
     _expression: ($, previous) => {
       return choice(
-          previous,
-          $.ellipsis,
-          $.deep_ellipsis
+        previous,
+        $.ellipsis,
+        $.deep_ellipsis
       );
+    },
+
+    main_component_definition: ($, previous) => {
+      return choice(
+        previous,
+        seq(
+          'component',
+          'main',
+          optional($.main_component_public_signals),
+          '=',
+          field('value', choice($.ellipsis, $.deep_ellipsis)),
+          $._semicolon
+    
+        ),
+      )
+    },
+
+    _component_declaration: ($, previous) => {
+      return choice(
+        previous,
+        seq(
+          field('name', $.identifier),
+          optional(field('type', $.array_type)),
+          optional(seq(
+            '=',
+            field('value', choice($.ellipsis, $.deep_ellipsis))
+          ))
+        )
+      )
     },
 
     expression_statement: ($, previous) => {
