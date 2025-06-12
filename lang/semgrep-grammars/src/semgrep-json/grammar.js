079c2f78085d04d5384f4f1505a3fce029d4870a
brandonspark <wu.brandonj@gmail.com>
2025-06-12 14:48:31 -0700

    add some tests

--- lang/semgrep-grammars/src/semgrep-json/grammar.js
+++ lang/semgrep-grammars/src/semgrep-json/grammar.js
@@ -17,8 +17,36 @@ module.exports = grammar(base_grammar, {
      if they're not already part of the base grammar.
   */
   rules: {
-  /*
+    document: ($, previous) => choice(
+      previous,
+      seq($.pair, optional(','))
+    ),
+
+    // Need high precedence so this is preferred over global_variable.
+    semgrep_metavariable: $ => token(/\$[A-Z_][A-Z_0-9]*/),
+
     semgrep_ellipsis: $ => '...',
+
+    object: $ => seq(
+      '{', commaSep($.pair), optional(','), '}',
+    ),
+
+    string: ($, previous) => choice(
+      previous,
+      $.semgrep_metavariable,
+    ),
+
+    _value: ($, previous) => prec(1, choice(
+      previous,
+      $.semgrep_ellipsis,
+    )),
+
+    pair: ($, previous) => choice(
+      previous,
+      seq($.semgrep_ellipsis)
+    ),
+
+  /*
 
     _expression: ($, previous) => choice(
       $.semgrep_ellipsis,
@@ -27,3 +55,25 @@ module.exports = grammar(base_grammar, {
   */
   }
 });
+
+/**
+ * Creates a rule to match one or more of the rules separated by a comma
+ *
+ * @param {RuleOrLiteral} rule
+ *
+ * @returns {SeqRule}
+ */
+function commaSep1(rule) {
+  return seq(rule, repeat(seq(',', rule)));
+}
+
+/**
+ * Creates a rule to optionally match one or more of the rules separated by a comma
+ *
+ * @param {RuleOrLiteral} rule
+ *
+ * @returns {ChoiceRule}
+ */
+function commaSep(rule) {
+  return optional(commaSep1(rule));
+}
