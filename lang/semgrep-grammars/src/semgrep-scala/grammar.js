4b6bd54b957d8d0f4b0b4acabda6fa8455d17129
brandonspark <wu.brandonj@gmail.com>
2025-10-15 14:47:30 -0400

    init for scala

---
+++ lang/semgrep-grammars/src/semgrep-scala/grammar.js
@@ -0,0 +1,29 @@
+/*
+  semgrep-scala
+
+  Extends the standard scala grammar with semgrep pattern constructs.
+*/
+
+const base_grammar = require('tree-sitter-scala/grammar');
+
+module.exports = grammar(base_grammar, {
+  name: 'scala',
+
+  conflicts: ($, previous) => previous.concat([
+  ]),
+
+  /*
+     Support for semgrep ellipsis ('...') and metavariables ('$FOO'),
+     if they're not already part of the base grammar.
+  */
+  rules: {
+  /*
+    semgrep_ellipsis: $ => '...',
+
+    _expression: ($, previous) => choice(
+      $.semgrep_ellipsis,
+      ...previous.members
+    ),
+  */
+  }
+});
