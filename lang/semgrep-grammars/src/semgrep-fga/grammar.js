b0d234085765d2cf726c521619b71f8da33096de
Alex Useche <alex.o.useche@gmail.com>
2025-11-12 11:36:33 -0800

    add support for openfga model parsing

---
+++ lang/semgrep-grammars/src/semgrep-fga/grammar.js
@@ -0,0 +1,29 @@
+/*
+  semgrep-fga
+
+  Extends the standard fga grammar with semgrep pattern constructs.
+*/
+
+const base_grammar = require('tree-sitter-fga/grammar');
+
+module.exports = grammar(base_grammar, {
+  name: 'fga',
+
+  conflicts: ($, previous) => previous.concat([
+  ]),
+
+  /*
+     Support for semgrep ellipsis ('...') and metavariables ('$FOO'),
+     if they're not already part of the base grammar.
+  */
+  rules: {
+  /*
+    semgrep_ellipsis: $ => '...',
+
+    _expression: ($, previous) => choice(
+      $.semgrep_ellipsis,
+      ...previous.members
+    ),
+  */
+  }
+});
