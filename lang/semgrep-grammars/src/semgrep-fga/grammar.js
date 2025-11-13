abd474cfa34f29c1864a3bb27b3fcba2f1d079e8
Alex Useche <alex.o.useche@gmail.com>
2025-11-13 00:39:13 -0800

    extended fga gramar for semgrep, added support for number lits for base grammar

--- lang/semgrep-grammars/src/semgrep-fga/grammar.js
+++ lang/semgrep-grammars/src/semgrep-fga/grammar.js
@@ -1,29 +1,80 @@
-/*
-  semgrep-fga
-
-  Extends the standard fga grammar with semgrep pattern constructs.
-*/
-
-const base_grammar = require('tree-sitter-fga/grammar');
+const base_grammar = require('../tree-sitter-fga/grammar');
 
 module.exports = grammar(base_grammar, {
   name: 'fga',
-
+  
   conflicts: ($, previous) => previous.concat([
+    [$.relations]
   ]),
-
-  /*
-     Support for semgrep ellipsis ('...') and metavariables ('$FOO'),
-     if they're not already part of the base grammar.
-  */
+  
   rules: {
-  /*
     semgrep_ellipsis: $ => '...',
 
-    _expression: ($, previous) => choice(
-      $.semgrep_ellipsis,
-      ...previous.members
+    semgrep_metavariable: $ => /\$[A-Z_][A-Z_0-9]*/,
+    
+    type_declaration: ($, previous) => seq(
+      optional('extend'),
+      'type',
+      choice($.identifier, $.semgrep_metavariable),
+      '\n',
+      optional($.relations)
     ),
-  */
+    
+    definition: ($, previous) => seq(
+      'define',
+      field('relation', choice($.identifier, $.semgrep_metavariable)),
+      ':',
+      $.relation_def
+    ),
+    
+    _module_file: ($, previous) => seq(
+      choice($.model, $.module),
+      repeat(choice(
+        $.type_declaration,
+        $.condition_declaration,
+        $.semgrep_ellipsis
+      ))
+    ),
+    
+    relations: ($, previous) => seq(
+      'relations',
+      repeat(choice(
+        $.definition,
+        $.semgrep_ellipsis
+      ))
+    ),
+    
+    _expression: ($, previous) => choice(
+      ...previous.members,
+      $.semgrep_ellipsis,
+      $.semgrep_metavariable,
+    ),
+
+    param: ($, previous) => seq(
+      choice($.identifier, $.semgrep_metavariable),  // Allow $VAR
+      ':',
+      choice($.type_identifier, $.semgrep_metavariable)  // Allow $TYPE
+    ),
+    
+    direct_relationship: ($, previous) => seq(
+      '[',
+      choice(
+        seq(
+          seq(
+            choice($.identifier, $.relation_ref, $.all, $.semgrep_metavariable),
+            optional($.conditional)
+          ),
+          optional(repeat(seq(
+            ',',
+            seq(
+              choice($.identifier, $.relation_ref, $.all, $.semgrep_metavariable),
+              optional($.conditional)
+            )
+          )))
+        ),
+        $.semgrep_ellipsis
+      ),
+      ']'
+    ),
   }
 });
