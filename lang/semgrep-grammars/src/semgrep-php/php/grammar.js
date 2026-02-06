0ff39cc2a24c610ace60a1a378300b327dd0f310
Cooper Pierce <cooper@semgrep.com>
2026-02-06 16:31:56 -0500

    feat: semgrep pattern extensions for php

--- lang/semgrep-grammars/src/semgrep-php/php/grammar.js
+++ lang/semgrep-grammars/src/semgrep-php/php/grammar.js
@@ -8,7 +8,13 @@ const php_grammar = require('tree-sitter-php/php/grammar');
 
 const semgrepExt = require('../common/semgrep-ext');
 
+// Delete the `word`, since it is set to `name`, which we need to make name a
+// non-terminal production (to allow for it to be a choice between metavariable
+// and the prior `name` production). But the rule used for `word` (for keyword
+// extraction) cannot be a non-terminal.
+delete php_grammar.grammar.word;
+
 module.exports = grammar(php_grammar, {
-  name: 'php',
-  ...semgrepExt,
+    name: 'php',
+    ...semgrepExt,
 });
